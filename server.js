162e26564e0381d94542ffff879071f6e20bf2b2
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-28 10:49:13 -0800

    coded the post contact route using nodemailer and tested it through postman

--- server.js
+++ server.js
@@ -21,6 +21,37 @@ app.get('/contact', ( req, res ) => {
 	res.send({ title: 'Contact page' });
 });
 
+app.post('/contact/send', ( req, res ) => {
+	let transporter = nodeMailer.createTransport( {
+		service: 'Gmail',
+		auth: {
+			user: 'ahmedanwar.soliman@gmail.com',
+			pass: '***'
+		}
+	});
+
+	let name  	= req.body.name;
+	let email 	= req.body.email;
+	let message = req.body.message;
+
+	let mailOptions = {
+		from: 'Ahmed Soliman <ahmed@firewoodmarketing.com>',
+		to: 'ahmed.soliman@email.com',
+		subject: 'Website Submission',
+		text: `You have a submission with the following details... Name: ${name}, Email: ${email}, Message: ${message}.`,
+		html: `<p>You have a submission with the following details...</p> <ul><li><b>Name: </b>${name}</li><li><b>Email: </b>${email}</li><li><b>Message: </b>${message}</li></ul>`
+	};
+
+	transporter.sendMail(mailOptions, ( err, info ) => {
+		if ( err ) {
+			console.log(err);
+			return res.status(401).send({message: 'Not able to send email...'})
+		}
+		console.log('Message Send: ' + info.response);
+		res.status(200).send({message: `Message Sent : ${info.response}`});
+	});
+});
+
 app.listen(port, () => {
 	console.log(`App is listening on port ${port}.`);
 });
